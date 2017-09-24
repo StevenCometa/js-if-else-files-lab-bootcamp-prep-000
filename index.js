3fdd4e662b4b4dbacf6e09d790c84ca5f46dcef5
Steven Cometa <sdcometa@gmail.com>
2017-09-24 09:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var height = "74"
 module.exports = { name, height
 }
 
-if (name === "susan") {
-    console.log("the name is correct")
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
 }
